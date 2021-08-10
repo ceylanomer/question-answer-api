6a45e7a502bab41ff92aa2201cb9b6efca7804b5
Ömer Ceylan <omer-ceylan@hotmail.com>
2021-08-10 17:26:10 +0300

    expressjs projects

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require("express")
+const dotenv = require("dotenv")
+
+
+dotenv.config({
+    path: "./config/env/config.env"
+});
 
 const app = express();
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT;
+
+app.get("/api/questions", (req, res) => {
+    res.send("Questions Home Page")
+})
+
+app.get("/api/questions/delete", (req, res) => {
+    res.send("Questions Delete Page")
+})
+
+app.get("/api/auth", (req, res) => {
+    res.send("Questions Home Page")
+})
+
+app.get("/api/auth/register", (req, res) => {
+    res.send("Questions Delete Page")
+})
 
 app.listen(PORT, ()=>{
-    console.log(`App started on ${PORT}`);
+    console.log(`App started on ${PORT} : ${process.env.NODE_ENV}`);
 })
