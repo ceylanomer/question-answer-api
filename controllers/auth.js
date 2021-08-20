4ab8e1d5a77059ba2949aa5efd3de500a9932d1f
Ömer Ceylan <omer-ceylan@hotmail.com>
2021-08-20 13:02:12 +0300

    düzenlemeler

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,27 @@
-
 const User =  require('../models/User')
+const CustomError = require('../helpers/error/CustomError');
 const register = async (req, res, next) => {
   //POST DATA
   const name = "Simge Ataman";
   const email = "simgeataman@gmail.com";
-  const password = "123456"
+  const password = "12356"
+
   
-  const user = await User.create({
-    name,
-    email,
-    password
-  });
   
-  res.status(200).json({
-    success: true,
-    data : user
-  });
+  try {
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+    
+    res.status(200).json({
+      success: true,
+      data : user
+    });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const connectDatabase = require("./helpers/database/connectDatabase")
+const customErrorHandler = require("./middlewares/errors/customErrorHandler")
 const routers = require("./routers")
 
 // Environment Variable
@@ -17,7 +18,7 @@ const PORT = process.env.PORT;
 
 //Routers Middleware
 app.use("/api", routers)
-
+app.use(customErrorHandler);
 
 app.listen(PORT, ()=>{
     console.log(`App started on ${PORT} : ${process.env.NODE_ENV}`);
